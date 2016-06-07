06555c238fda3a1de1be390575e12ce2b4aae987
emilyvivancocuba <emilyvivancocuba@gmail.com>
2016-06-07 16:32:19 -0500

    nivel 0 terminado, nivel 1 buscador de contacto terminado

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ $(document).ready(function(){
 		});
 		$('#title_each_contact').html('Ana María Martinez Franklin');
 		repeat();
+		$('#bg-panel').html('<div class="mensajes msg-left"><div><span class="msg-contact">Que tal chama, cómo estás</span></div><div><span class="hora-2">12:10</span></div></div>');
 	});
 	$('#rodulfo').on('click', function(){
 		$('#foto').attr({
@@ -46,6 +47,7 @@ $(document).ready(function(){
 		});
 		$('#title_each_contact').html('Rodulfo Prieto');
 		repeat();
+		$('#bg-panel').html('<div class="mensajes msg-left"><div><span class="msg-contact">Suerte a todas con el examen</span></div><div><span class="hora-2">2:15</span></div></div><div class="mensajes msg-left"><div><span class="msg-contact">Me han dicho que está re-difícil</span></div><div><span class="hora-2">2:17</span></div></div>');
 	});
 	$('#andrea').on('click', function(){
 		$('#foto').attr({
@@ -55,6 +57,7 @@ $(document).ready(function(){
 		});
 		$('#title_each_contact').html('Andrea Lamas');
 		repeat();
+		$('#bg-panel').html('<div class="mensajes msg-left"><div><span class="msg-contact">Te faltan corregir algunas tildes en tu CV</span></div><div><span class="hora-2">3:20</span></div></div><div class="mensajes msg-right"><div><span class="msg-contact">Gracias por las recomendaciones, Andrea</span></div><div><span class="hora-2">3:34</span></div></div>');
 	});
 	$('#mariapaula').on('click', function(){
 		$('#foto').attr({
@@ -64,6 +67,7 @@ $(document).ready(function(){
 		});
 		$('#title_each_contact').html('Maria Paula Rivarola');
 		repeat();
+		$('#bg-panel').html('<div class="mensajes msg-left"><div><span class="msg-contact">Ya te reuniste con tu mentor?</span></div><div><span class="hora-2">7:30</span></div></div><div class="mensajes msg-right"><div><span class="msg-contact">Sí! Joe te va a pasar la foto</span></div><div><span class="hora-2">7:48</span></div></div>');
 	});
 	$('#katy').on('click', function(){
 		$('#foto').attr({
@@ -73,6 +77,7 @@ $(document).ready(function(){
 		});
 		$('#title_each_contact').html('Katy Sanchez');
 		repeat();
+		$('#bg-panel').html('<div class="mensajes msg-left"><div><span class="msg-contact">¿Qué te parece el examen?</span></div><div><span class="hora-2">3:20</span></div></div><div class="mensajes msg-right"><div><span class="msg-contact">Ayúdame, Katy, por favor :´&#40</span></div><div><span class="hora-2">3:34</span></div></div>');
 	});
 	$('#aldo').on('click', function(){
 		$('#foto').attr({
@@ -82,18 +87,42 @@ $(document).ready(function(){
 		});
 		$('#title_each_contact').html('Aldo Alfaro');
 		repeat();
+		$('#bg-panel').html('<div class="mensajes msg-left"><div><span class="msg-contact">Dale like a las publicaciones en Laboratoria</span></div><div><span class="hora-2">3:20</span></div></div><div class="mensajes msg-right"><div><span class="msg-contact">Listo!</span></div><div><span class="hora-2">3:34</span></div></div>');
 	});
 });
-
-
 
 function repeat(){
 	$('#subtitle_each_contact').html('<br>');
 }
 
+var horas, minutos;
+
+function hora_peru(){
+	var datos=new Date();
+   	horas=datos.getHours();
+   	minutos=datos.getMinutes();
+
+   	if (horas<10) {horas='0'+horas.string();}
+   	if (minutos<10) {minutos='0'+minutos.string();}
+}
+
+function doMsg(texto){
+	hora_peru();
+
+	var elemento=$('#bg-panel').append('<div class="mensajes msg-right"><div><span class="msg-contact">'+texto+'</span></div><div><span class="hora-2">'+horas+':'+minutos+'</span></div></div>');
+}
+
+
+
 function escribir(){
 	var cuerpo_del_mensaje = $('#input_del_mensaje');
-	return cuerpo_del_mensaje.value;	
+	return cuerpo_del_mensaje.val();	
+}
+
+function clean(){
+	var cuerpo_del_mensaje = $('#input_del_mensaje');
+	cuerpo_del_mensaje.val("");
+	cuerpo_del_mensaje.focus();
 }
 
 function decide(){
@@ -103,12 +132,87 @@ function decide(){
 		clean();
 	}
 }
-function doMsg(texto){
-	var elemento=$('#bg-panel').append('<div class="mensajes msg-right"><div><span class="msg-contact">'+texto+'</span></div><div><span class="hora-2">11:14</span></div></div>');
+
+$('#input_del_mensaje').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        decide();
+    }
+});
+
+function escribir1(){
+	var texto_busqueda = $('#input_de_busqueda');
+	return texto_busqueda.val().toLowerCase();	
 }
 
-function clean(){
-	var cuerpo_del_mensaje = $('#input_del_mensaje');
-	cuerpo_del_mensaje.value='';
-	cuerpo_del_mensaje.focus();
+
+var nombres_buscar=['laboratoria', 'raymi', 'saldomando', 'mariana', 'costa', 'ana', 'maria', 'martinez', 'Franklin', 'rodulfo', 'prieto', 'andrea', 'lamas', 'maria', 'paula', 'Rivarola', 'katy', 'sanchez', 'aldo', 'alfaro'];
+
+
+var ids=['laboratoria', 'raymi', 'mariana', 'ana', 'rodulfo', 'andrea', 'mariapaula', 'katy', 'aldo']
+
+function mostrar(contacto, numero){
+	var variable=contacto;
+	for (var j = 0; j < ids.length; j++) {
+		if (j!==numero) {
+			
+			$('#'+ids[j]).css('display', 'none');
+		}else{
+			$('#'+variable).css('display', 'block !important');
+		}
+	}
 }
+
+function buscar(){
+	for (var i = 0; i < nombres_buscar.length; i++) {
+		if (texto_busqueda_valor==nombres_buscar[i]) {
+			var k=i;
+			if (k===0) {
+				mostrar('laboratoria', 0);
+			}
+			else if (k==1||k==2) {
+				mostrar('raymi', 1);
+			}else if (k==3||k==4) {
+				mostrar('mariana', 2);
+			}else if (k==5||k==6||k==7||k==8) {
+				mostrar('ana', 3);
+			}else if (k==9||k==10) {
+				mostrar('rodulfo', 4);
+			}else if (k==11||k==12) {
+				mostrar('andrea', 5);
+			}else if (k==13||k==14||k==15) {
+				mostrar('mariapaula', 6);
+			}else if (k==16||k==17) {
+				mostrar('katy', 7);
+			}else if (k==18||k==19) {
+				mostrar('aldo', 8);
+			}else{
+				$('div.contact-zone').html('No se encontró ningún contacto que coincida con su búsqueda');
+			}
+		}
+	}
+}
+
+function clean1(){
+	var texto_busqueda = $('#input_de_busqueda');
+	texto_busqueda.val("");
+	texto_busqueda.focus();
+}
+
+
+var texto_busqueda_valor;
+function decide1(){
+	texto_busqueda_valor=escribir1();
+	if (texto_busqueda_valor!=='') {
+		buscar();
+		clean1();
+	}
+}
+
+$('#input_de_busqueda').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        decide1();
+    }
+});
+
